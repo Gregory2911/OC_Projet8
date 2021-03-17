6fa87a1b3f27f16482e05ed551d82360b270ae27
Grégory AGNAN <agnan.gregory@orange.fr>
2021-03-17 22:18:26 +0100

    implementation of unit tests of User entity

--- tests/Task/TaskTest.php
+++ tests/Task/TaskTest.php
@@ -35,12 +35,12 @@ class TaskTest extends KernelTestCase
         $this->assertHasErrors($this->getEntity(), 0);
     }
 
-    public function testInvalidTitle()
+    public function testInvalidBlankTitle()
     {
         $this->assertHasErrors($this->getEntity()->setTitle(""), 1);
     }
 
-    public function testInvalidContent()
+    public function testInvalidBlankContent()
     {
         $this->assertHasErrors($this->getEntity()->setContent(""), 1);
     }
---
+++ tests/User/UserTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Tests\User;
+
+use App\Entity\User;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class UserTest extends KernelTestCase
+{
+
+    public function getEntity()
+    {
+        return (new User())
+            ->setUsername("Grégory")
+            ->setPassword('password')
+            ->setEmail("test@test.fr");
+    }
+
+    public function assertHasErrors(User $task, int $number = 0)
+    {
+        self::bootKernel();
+        $errors = self::$container->get('validator')->validate($task);
+        $messages = [];
+        /**@var ConstraintViolation $error */
+        foreach ($errors as $error) {
+            $messages[] = $error->getPropertyPath() . ' => ' . $error->getMessage();
+        }
+        $this->assertCount($number, $errors, implode(', ', $messages));
+    }
+
+    public function testValidEntity()
+    {
+        $this->assertHasErrors($this->getEntity(), 0);
+    }
+
+    public function testInvalidBlankUsername()
+    {
+        $this->assertHasErrors($this->getEntity()->setUsername(""), 1);
+    }
+
+    public function testInvalidBlankPassword()
+    {
+        $this->assertHasErrors($this->getEntity()->setPassword(""), 1);
+    }
+
+    public function testInvalidBlankEmail()
+    {
+        $this->assertHasErrors($this->getEntity()->setEmail(""), 1);
+    }
+
+    public function testInvalidEmail()
+    {
+        $this->assertHasErrors($this->getEntity()->setEmail("test@test"), 1);
+    }
+
+}
