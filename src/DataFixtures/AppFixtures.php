978e3f177945e9034c3f48f02bc557420f9986f7
Gregory2911 <agnan.gregory@orange.fr>
2021-03-19 14:55:33 +0100

    creation of fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Task;
+use Faker\Factory;
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+
+        //Creation of the admin user
+        $admin = new User();
+        $admin->setUsername('admin');
+        $admin->setEmail('admin@admin.com');
+        $admin->setPassword($this->encoder->encodePassword($admin, "password"));
+        $admin->setRoles(array('ROLE_ADMIN'));
+
+        $manager->persist($admin);
+
+        $users = array();
+        
+        //Creation of 5 users
+        for($i = 0; $i < 5; $i++){
+            $user = new User();
+            $user->setUsername($faker->userName());
+            $user->setPassword($this->encoder->encodePassword($user, $user->getUsername()));
+            $user->setEmail($user->getUsername() . '@' . $faker->safeEmailDomain());
+            $user->setRoles(array('ROLE_USER'));
+            $users[] = $user;
+
+            $manager->persist($user);
+        }
+
+        //Creation of 10 tasks
+        for($i = 0; $i < 10; $i++){
+            $task = new Task();
+            $task->setTitle($faker->sentence(6));
+            $task->setContent($faker->paragraph());
+            $task->setCreatedAt($faker->dateTimeBetween('-2 months', '-1 months'));
+            $task->setUser($users[mt_rand(0, count($users)-1)]);
+            $task->isDone(0);
+            
+            $manager->persist($task);
+        }
+
+        $manager->flush();
+    }
+}
--- tests/Entity/TaskTest.php
+++ tests/Entity/TaskTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Entity;
 
 use App\Entity\Task;
+use App\Entity\User;
 use Symfony\Component\Validator\ConstraintViolation;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
@@ -12,10 +13,12 @@ class TaskTest extends KernelTestCase
 
     public function getEntity()
     {
+        $user = new User();
         return (new Task())
             ->setTitle("Ceci est un titre test")
             ->setContent('Ceci est un contenu de test')
-            ->setCreatedAt(new \DateTimeImmutable());
+            ->setCreatedAt(new \DateTimeImmutable())
+            ->setUser($user);
     }
 
     public function assertHasErrors(Task $task, int $number = 0)
@@ -45,4 +48,8 @@ class TaskTest extends KernelTestCase
         $this->assertHasErrors($this->getEntity()->setContent(""), 1);
     }
 
+    public function testInvalidUser()
+    {
+        $this->assertHasErrors($this->getEntity()->setUser(null), 1);
+    }
 }
