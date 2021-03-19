c750fb526fa34dea5d0deb75aa16425e23e8145d
Gregory2911 <agnan.gregory@orange.fr>
2021-03-19 17:39:32 +0100

    start of the implementation of functional tests on the controllers

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -5,7 +5,6 @@ namespace App\Tests\Controller;
 use App\Tests\LoginForTest;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
-use Liip\TestFixturesBundle\Test\FixturesTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class TaskControllerTest extends WebTestCase
@@ -29,25 +28,26 @@ class TaskControllerTest extends WebTestCase
     public function testTasksEditPage()
     {
         $client = static::createClient();
-        $client->request('GET', '/tasks/5/edit');
+        $client->request('GET', '/tasks/11/edit');
         $this->assertResponseStatusCodeSame(Response::HTTP_OK);
     }
 
     public function testTasksTogglePage()
     {
         $client = static::createClient();
-        $client->request('GET', '/tasks/5/toggle');
+        $client->request('GET', '/tasks/11/toggle');
         $this->assertResponseRedirects('/tasks');
     }
 
     public function testTasksDeletePageWithBadUser()
     {
         $client = static::createClient();
-        $client->request('GET', '/tasks/5/delete');
+        $client->request('GET', '/tasks/11/delete');
         $userRepository = static::$container->get(UserRepository::class);
         $user = $userRepository->findOneByUsername('admin');
         $this->login($client, $user);
         $this->assertResponseRedirects('/tasks');
         $this->assertSelectorExists('.alert.alert-danger');
+        //voir si dans la liste des taches la tache existe encore
     }
 }
